fb1af6b7b24ac61a3b1a10b58f872acc8e8fb6e5
Carla022 <cs.morenol@outlook.com>
2022-09-28 09:23:05 +0100

    add css + delete function in completed list

--- lab_jsevents_start_code/script.js
+++ lab_jsevents_start_code/script.js
@@ -1,21 +1,36 @@
 // selectors
+// with the query "getElementById", you dont need to add the hash symbol as it already knows that it is looking
 const enter = document.querySelector("#enter")
 const list = document.querySelector('#list')
 const newTodo = document.querySelector('#new-todo')
 const completeList = document.querySelector('#list-complete')
 
+
 //click button
-enter.addEventListener('click', () => {
+enter.addEventListener('click',  () => {
     //create list
     const li = document.createElement('li')
     li.innerText = newTodo.value;
     //adding to the list
     list.append(li) 
+   
+    // if no user input in form, prompt user input else, add to the list{
+
+
 
     //create completelist checkbox
     const checkbox = document.createElement('input')
     checkbox.setAttribute('type', 'checkbox')
+    //get the list and apply the checkbox:
     li.append(checkbox)
+
+    //transfer unchecked li back to toDo list:
+    checkbox.addEventListener('change', () => {
+        completeList.removeChild(li)
+        //transfer to list
+        list.append(li)
+    })
+
 
     //transfer to completedlist
     checkbox.addEventListener('change', () => {
@@ -35,8 +50,13 @@ enter.addEventListener('click', () => {
         list.removeChild(li) 
     });
 
+    //add delete funciton to completeList li
+    removeBtn.addEventListener('click', () => {
+        completeList.removeChild(li) 
+    });
 
 
+    //create show date button
     const showDateBtn = document.createElement('button')
     showDateBtn.innerText = 'Show Date';
 
@@ -44,11 +64,12 @@ enter.addEventListener('click', () => {
 
     showDateBtn.addEventListener('click', () => {
         const date = Date()
-        // append variable date
+        // append variable date - add it to the li
         li.append(date)
         //to disable button after click
         showDateBtn.disabled = true
     });
+
 
 
 
